8fcb911563d28f0b55b2a1ad9b4e3a105a5f5e5b
Ankur Seth <iankurseth@gmail.com>
2022-01-10 12:33:53 +0530

    Replaced all custom CSS with tailwind classes

--- src/FloatingWidget.js
+++ src/FloatingWidget.js
@@ -4,7 +4,7 @@ import './FloatingWidget.css'
 
 function FloatingWidget ({color}) {
 	return (
-		<div className="FloatingWidget">
+		<div className="FloatingWidget px-6 py-4 bg-white text-slate-800 fixed bottom-4 right-4 border-2 border-slate-400">
 			<div className="hexColor">{color}</div>
 		</div>
 	);
--- src/Footer.js
+++ src/Footer.js
@@ -4,9 +4,9 @@ import './Footer.css'
 
 function Footer () {
 	return (
-		<footer className="Footer text-white py-20 px-4">
+		<footer className="Footer bg-purple-800 text-center text-white py-20 px-4">
 			<div className="Copyright">
-				<h4>&copy; Copyright 2022, Ankur Seth (<a href="https://github.com/iaseth">GitHub</a>).</h4>
+				<h4>&copy; Copyright 2022, Ankur Seth (<a href="https://github.com/iaseth" class="text-blue-300 p-1">GitHub</a>).</h4>
 			</div>
 		</footer>
 	);
