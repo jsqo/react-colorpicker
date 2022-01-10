d3516eedb1a1486c7abdb89c28f0a398f744e4e0
Ankur Seth <iankurseth@gmail.com>
2022-01-10 14:56:03 +0530

    Added PreFooter with colorboxes

--- src/Slides/Slides.js
+++ src/Slides/Slides.js
@@ -12,10 +12,33 @@ function Slides ({color}) {
 		</Fragment>
 	);
 
+	let preFooterSlideBoxItems = slides.colors.map((slideColor) => {
+		let style = {
+			"backgroundColor": slideColor.code
+		};
+		return (
+			<div className="SlideBox w-40 m-4 inline-block border-2">
+				<div class="p-2 font-bold">{slideColor.name}</div>
+				<div className="h-24" style={style}></div>
+				<div class="p-2">
+					<code>{slideColor.code}</code>
+				</div>
+			</div>
+		);
+	});
+
 	return (
-		<div className="Slides">
-			{slideItems}
-		</div>
+		<Fragment>
+			<div className="Slides">
+				{slideItems}
+			</div>
+			<div className="PreFooter bg-white px-20 py-40 text-center">
+				<h2 className="text-4xl p-10">List of colors:</h2>
+				<ul>
+					{preFooterSlideBoxItems}
+				</ul>
+			</div>
+		</Fragment>
 	);
 }
 
